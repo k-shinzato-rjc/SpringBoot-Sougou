bf17bfa6b60bd094e8c684ec6ad9266ac9c8f08d
RJC\koki_shinzato <koki_shinzato@r-jc.jp>
2026-05-27 08:18:08 +0900

    SpringBoot総合問題 メンバー一覧画面 新里光貴

--- sougou/src/main/java/com/example/demo/controller/MemberController.java
+++ sougou/src/main/java/com/example/demo/controller/MemberController.java
@@ -1,15 +1,8 @@
 package com.example.demo.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import com.example.demo.dto.MemberDto;
-import com.example.demo.service.MemberService;
 
 
 /**
@@ -18,27 +11,6 @@ import com.example.demo.service.MemberService;
  */
 @Controller
 public class MemberController {
-	
-	@Autowired
-	MemberService memberService;
-	
-	/**
-	 * メンバー一覧画面を表示
-	 * @param model
-	 * @return
-	 */
-	@GetMapping("/list")
-	public String list(Model model) {
-		
-		// DBからデータを取得
-		List<MemberDto> memberDto = memberService.findAll();
-		
-		// Formに変換してViewへ渡す
-		model.addAttribute("members", memberService.convertToForm(memberDto));
-		
-		// list.htmlに遷移
-		return "list/list";
-	}
 
 	/**
 	 * 実装予定
--- sougou/src/main/java/com/example/demo/controller/MenuController.java
+++ sougou/src/main/java/com/example/demo/controller/MenuController.java
@@ -1,7 +1,14 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.dto.MemberDto;
+import com.example.demo.service.MemberService;
 
 /**
  * Menu画面の管理するコントローラー
@@ -9,6 +16,9 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class MenuController {
+	
+	@Autowired
+	private MemberService memberService;
 	
 	/**
 	 * 初期メニュー画面の表示
@@ -32,5 +42,23 @@ public class MenuController {
 		
 		return "insert/insert";
 		
+	}
+	
+	/**
+	 * メンバー一覧画面を表示
+	 * @param model
+	 * @return
+	 */
+	@GetMapping("/list")
+	public String list(Model model) {
+		
+		// DBからデータを取得
+		List<MemberDto> memberDto = memberService.findAll();
+		
+		// Formに変換してViewへ渡す
+		model.addAttribute("members", memberService.convertToForm(memberDto));
+		
+		// list.htmlに遷移
+		return "list/list";
 	}
 }
